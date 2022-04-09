76b3eb517e9b9002258f4ad69412a6398012f4a6
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-04-09 21:50:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<h1>Hệ thống quản lý sinh viên</h1>
 	<?php
 	$cn = pg_connect("host=ec2-18-214-134-226.compute-1.amazonaws.com port=5432 dbname=d5tmin00bciu1i user=zmqcfqvmpyljhr password=9f82098da86027af5990973f6e6f36ad470852bdd6b70a9bccd9cb6cb603495d");
-	$result = pg_query($cn,"select Tensinhvien where HTQL_QuanLySinhVien WHERE Mssv='B1809579'");
+	$result = pg_query($cn,"select Tensinhvien from HTQL_QuanLySinhVien WHERE Mssv='B1809579'");
 	while($row = pg_fetch_object($result)){
 		echo "\n".$row->Tensinhvien;
 	}
