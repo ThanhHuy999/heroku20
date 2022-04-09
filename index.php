29c83f044cddfea9851985633522ec0aa48ab143
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-04-09 22:11:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	$result = pg_query($cn,"SELECT * FROM HTQL_QuanLySinhVien");
 	while($row= pg_fetch_object($result))
 	{
-		echo "\n".$row->Tensinhvien;
+		echo "\n".$row->Mssv;
 	}
 	?>
 	</body>
