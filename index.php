14452db2da282be860ff53bd17ee30768cfe32ed
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-04-11 21:00:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,54 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<meta charset="utf-8">
-	<title> He thong quan ly sinh vien </title>
+		<meta charset="utf-8">
+		<title> He thong quan ly sinh vien </title>
 	</head>
 
 	<body>
-	<h1>Hệ thống quản lý sinh viên</h1>
-	<form name="flogin" method="POST" action="xuly.php"
-	      <pre>
-		Mã số sinh viên: <input type="text" name="uname">
-		<br>
-		Họ và tên: <input type="text" name="pword">
-		<br>
-		<input type="submit" name="them" value="THÊM">
-		<input type="button" name="sua" value="SỬA">
-		<input type="button" name="xoa" value="XÓA">
-		<hr>
-		</pre>
-	      </form>
-	<?php
-	$cn = pg_connect("host=ec2-18-214-134-226.compute-1.amazonaws.com port=5432 dbname=d5tmin00bciu1i user=zmqcfqvmpyljhr password=9f82098da86027af5990973f6e6f36ad470852bdd6b70a9bccd9cb6cb603495d");
-	$result = pg_query($cn,"SELECT * FROM HTQL_QuanLySinhVien");
-	while($row= pg_fetch_row($result))
-	{
-		echo "MSSV: $row[0] Email: $row[1]";
-		echo "\n";
-	}
-	pg_close($cn);
-	?>
+		<h1>Hệ thống quản lý sinh viên</h1>
+			<h2>Thêm sinh viên</h2>
+				<form name="add" method="POST" action="xuly.php"
+				      <pre>
+						Mã số sinh viên: <input type="text" name="uname">
+						<br>
+						Họ và tên: <input type="text" name="pword">
+						<br>
+						<input type="submit" name="them" value="THÊM">
+						<hr>
+					</pre>
+				  </form>
+			<hr>
+			<h2>Sửa nhân viên</h2>
+				<form name="update" method="POST" action="sua.php"
+				      <pre>
+						Mã số sinh viên cần sửa thông tin: <input type="text" name="uname">
+						<br>
+						Họ và tên: <input type="text" name="pword">
+						<br>
+						<input type="submit" name="sua" value="SỬA">
+						<hr>
+					</pre>
+				  </form>
+			<hr>
+			<h2>Xóa nhân viên</h2>
+				<form name="delete" method="POST" action="xoa.php"
+				      <pre>
+						Mã số sinh viên cần XÓA: <input type="text" name="uname">
+						<hr>
+					</pre>
+				  </form>
+			<h2>DANH SÁCH SINH VIÊN HIỆN TẠI</h2>
+
+		<?php
+			$cn = pg_connect("host=ec2-18-214-134-226.compute-1.amazonaws.com port=5432 dbname=d5tmin00bciu1i user=zmqcfqvmpyljhr password=9f82098da86027af5990973f6e6f36ad470852bdd6b70a9bccd9cb6cb603495d");
+			$result = pg_query($cn,"SELECT * FROM HTQL_QuanLySinhVien");
+			while($row= pg_fetch_row($result))
+			{
+				echo "MSSV: $row[0] Email: $row[1] \n";
+			}
+			pg_close($cn);
+		?>
 	</body>
 </html>
 
