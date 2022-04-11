0baa201545ec2c241a858a5bdb02c48d791cf35e
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-04-11 21:34:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -36,6 +36,8 @@
 				<form name="delete" method="POST" action="xoa.php"
 				      <pre>
 						Mã số sinh viên cần XÓA: <input type="text" name="uname">
+						<br>
+						<input type="submit" name="xoa" value="XÓA">
 						<hr>
 					</pre>
 				  </form>
