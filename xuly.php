245efc8efdc43b4d5c088582f186a1e175e4b67d
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-04-11 15:06:14 +0700

    Update xuly.php

--- xuly.php
+++ xuly.php
@@ -2,13 +2,14 @@
 $cn = pg_connect("host=ec2-18-214-134-226.compute-1.amazonaws.com port=5432 dbname=d5tmin00bciu1i user=zmqcfqvmpyljhr password=9f82098da86027af5990973f6e6f36ad470852bdd6b70a9bccd9cb6cb603495d");
 $name = $_POST["uname"];
 $hovaten = $_POST["pword"]
-$query1 = "INSERT INTO HTQL_QunaLySinhVien VALUES ('$name', '$hovaten')";
+$query1 = "INSERT INTO HTQL_QuanLySinhVien VALUES ('$name', '$hovaten')";
 $result = pg_query($cn, $query1 );
 if($result){
 	echo "Saved";
 }
 echo "Welcome " . $_POST['uname'] . "<br>";
 echo "Your email address is:" . $_POST['pword'];
+pg_close($cn);
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,6 +41,7 @@ echo "Your email address is:" . $_POST['pword'];
 		echo "MSSV: $row[0] Email: $row[1]";
 		echo "\n";
 	}
+	pg_close($cn);
 	?>  
 </body>
 </html>
